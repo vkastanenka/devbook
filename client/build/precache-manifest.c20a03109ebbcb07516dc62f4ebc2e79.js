e00e034c5004914365d30cdd29467e86f4daa605
Victoria Kastanenka <vkastanenka@gmail.com>
2020-06-12 14:00:34 -0400

    built a working build of client side code

---
+++ client/build/precache-manifest.c20a03109ebbcb07516dc62f4ebc2e79.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731059824b6f4b9fd9da7d8b5bc54308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d06a484fdfc3be97e61",
+    "url": "/static/css/main.d45ea74a.chunk.css"
+  },
+  {
+    "revision": "f7544b00b2e5b8d50258",
+    "url": "/static/js/2.a8036ac4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a8036ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d06a484fdfc3be97e61",
+    "url": "/static/js/main.3276092d.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "5881650b8ae558b7aee30f0c288bfbbc",
+    "url": "/static/media/default.5881650b.jpg"
+  },
+  {
+    "revision": "daa5787c2b7eaa390d1d95d6a365b67d",
+    "url": "/static/media/icons.daa5787c.svg"
+  },
+  {
+    "revision": "d30cda6e7f0031992928fd0490e1c7c1",
+    "url": "/static/media/user-1.d30cda6e.jpg"
+  },
+  {
+    "revision": "4aa8fa6a1fa3ffdb8c11e77fa816d872",
+    "url": "/static/media/user-10.4aa8fa6a.jpg"
+  },
+  {
+    "revision": "b2f692e61e616209252a0b7acac4b6e2",
+    "url": "/static/media/user-17.b2f692e6.jpg"
+  },
+  {
+    "revision": "8769d25b83452bd17e3840ff5478295e",
+    "url": "/static/media/user-9.8769d25b.jpg"
+  },
+  {
+    "revision": "a5568a27fbdc57f869148513d4723d1f",
+    "url": "/static/media/wallpaper--body.a5568a27.jpg"
+  },
+  {
+    "revision": "dcec04392dda6361ffe28797967d6a5a",
+    "url": "/static/media/wallpaper--card.dcec0439.jpg"
+  },
+  {
+    "revision": "5142d206951488f080f348fb0c9cd49c",
+    "url": "/static/media/wallpaper--nav.5142d206.jpg"
+  }
+]);
